8e51ae9a0c6d38f560e9a2fdce8fa440ea1787dc
Amadej Kastelic <amadejkastelic7@gmail.com>
2026-05-17 18:10:45 +0200

    flake/inputs/hyprland: v0.55.0 -> v0.55.2

--- flake.nix
+++ flake.nix
@@ -92,7 +92,7 @@
     apple-fonts.url = "github:Lyndeno/apple-fonts.nix";
 
     hyprland = {
-      url = "github:hyprwm/Hyprland?ref=v0.55.0";
+      url = "github:hyprwm/Hyprland?ref=v0.55.2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
