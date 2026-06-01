aaf8ffe0e8ac550efe4f18b044f436add540a9d8
Amadej Kastelic <amadejkastelic7@gmail.com>
2026-06-01 12:59:25 +0200

    fix

--- modules/home/desktop-packages.nix
+++ modules/home/desktop-packages.nix
@@ -3,6 +3,7 @@
   den.aspects.desktop-packages.homeManager =
     {
       pkgs,
+      lib,
       ...
     }:
     {
@@ -11,16 +12,16 @@
           gnumake
           hoppscotch
         ])
-        ++ (with pkgs; [
-          pinentry-gnome3
-          mission-center
-          wineWow64Packages.wayland
-          ledger-live-desktop
-          qbittorrent-enhanced
-          gnome-disk-utility
-        ])
-        ++ [
-          inputs.proxsign.packages.${pkgs.stdenv.hostPlatform.system}.default
-        ];
+        ++ lib.optionals (!pkgs.stdenv.isDarwin) (
+          (with pkgs; [
+            pinentry-gnome3
+            mission-center
+            wineWow64Packages.wayland
+            ledger-live-desktop
+            qbittorrent-enhanced
+            gnome-disk-utility
+          ])
+          ++ [ inputs.proxsign.packages.${pkgs.stdenv.hostPlatform.system}.default ]
+        );
     };
 }
