aec5135b7093b525256823c713371017550680ba
Amadej Kastelic <amadejkastelic7@gmail.com>
2026-07-01 20:33:25 +0200

    home/programs/discord: remove titlebar

--- home/programs/social/discord.nix
+++ home/programs/social/discord.nix
@@ -11,12 +11,14 @@
     };
 
     config = {
+      useQuickCss = true;
       themeLinks = [
         "https://raw.githubusercontent.com/catppuccin/discord/main/themes/mocha.theme.css"
       ];
       enabledThemes = [
         "Catppuccin Mocha"
       ];
+      frameless = true;
 
       plugins = {
         crashHandler = {
@@ -29,5 +31,40 @@
         messageLogger.enable = true;
       };
     };
+
+    # Remove titlebar
+    quickCss = ''
+      .bar_c38106 {
+        display: none !important;
+        height: 0 !important;
+        min-height: 0 !important;
+        max-height: 0 !important;
+        padding: 0 !important;
+        margin: 0 !important;
+        opacity: 0 !important;
+        pointer-events: none !important;
+        border: none !important;
+        overflow: hidden !important;
+      }
+
+      [class^="app-"] > [class^="sidebar_"] > [class^="container_"] > div:first-child {
+        display: none !important;
+        height: 0 !important;
+      }
+
+      [class^="base_"] {
+        top: 0 !important;
+        padding-top: 0 !important;
+        margin-top: 0 !important;
+      }
+
+      [class^="content_"] {
+        margin-top: 0 !important;
+      }
+
+      .platform-win .layer__960e4 {
+        top: -53px;
+      }
+    '';
   };
 }
